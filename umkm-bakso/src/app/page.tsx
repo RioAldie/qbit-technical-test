5450ac4b9072637de5b5a6b4c579f4d82722bd9e
rioaldie <rioaldierwanto@gmail.com>
2024-06-09 18:58:46 +0700

    founder profile

--- umkm-bakso/src/app/page.tsx
+++ umkm-bakso/src/app/page.tsx
@@ -1,12 +1,14 @@
 import CardMenu from '@/components/cardMenu';
 import Hero from '@/components/hero';
 import MenuSection from '@/components/menuSection';
+import Testimoni from '@/components/testimoni';
 
 export default function Home() {
   return (
     <main className=" p-24">
       <Hero />
       <MenuSection />
+      <Testimoni />
     </main>
   );
 }
